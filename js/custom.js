0ea09e89e3bd27417ffc82ee1f9815932ba2b099
Berat Bozkurt <beratbozkurt1999@gmail.com>
2018-05-17 14:56:26 +0300

    add

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($){
+
+	var uzunluk=1;
+	uzunluk++;
+	var sira=0;
+
+	$('.slider ul li:not(:eq('+sira+'))').hide();
+	$('.bsag').click(function(event){
+		$('.slider ul li').fadeOut();
+		if (sira<uzunluk) {
+			sira++;
+			$('.slider ul li:eq('+sira +')').fadeIn();
+		}
+		else {
+			sira=0;
+			$('.slider ul li:eq('+sira +')').fadeIn();
+		};
+	});
+
+	$('.bsol').click(function(event){
+		$('.slider ul li').fadeOut();
+		if (sira>0) {
+			sira--;
+			$('.slider ul li:eq('+sira +')').fadeIn();
+		}
+		else{
+			sira=uzunluk;
+			$('.slider ul li:eq('+sira +')').fadeIn();
+		}
+	})
+
+});
